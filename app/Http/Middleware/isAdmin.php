4a14f37b21cc95553ed30b6e5532b8e213fd483f
Elena Spasovska <spasovskaelena307@gmail.com>
2025-06-02 01:42:54 +0200

    minor change

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
+
 class isAdmin
 {
     /**
@@ -16,7 +17,7 @@ class isAdmin
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::user()->role !== 'admin') {
+        if (Auth::user()->role->name !== 'admin') {
             abort(403, 'Only admins can access this route.');
         }
 
